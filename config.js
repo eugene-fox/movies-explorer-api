2471c4bfcf0d602213bfaeef149ef74046e9788f
eugene-fox <69255033+eugene-fox@users.noreply.github.com>
2021-07-12 16:27:14 +0700

    fix/ change db name

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 const { PORT = 3000 } = process.env;
-const DB_ADDRESS = 'mongodb://localhost:27017/movies-explorer-db';
+const DB_ADDRESS = 'mongodb://localhost:27017/bitfilmsdb';
 
 module.exports = { PORT, DB_ADDRESS };
